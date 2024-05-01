db236f531bc3025d3e9cd9e9959590fa406efc36
Thang Vu <hi@thvu.dev>
2024-05-01 12:28:20 +0700

    upgrade

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,17 @@
-import { SvelteKitAuth, type SvelteKitAuthConfig } from "@auth/sveltekit"
-import GitHub from "@auth/core/providers/github"
-import type { Handle } from "@sveltejs/kit";
+import { SvelteKitAuth, type SvelteKitAuthConfig } from "@auth/sveltekit";
+import GitHub from "@auth/sveltekit/providers/github";
+import type { RequestEvent } from "@sveltejs/kit";
 
-export const handle = SvelteKitAuth(async (event) => {
+export const { handle } = SvelteKitAuth(async (event: RequestEvent) => {
   const authOptions = {
-    providers: [GitHub({ clientId: event.platform.env?.GITHUB_ID!, clientSecret: event.platform.env?.GITHUB_SECRET! })] as any,
+    providers: [
+      GitHub({
+        clientId: event.platform.env?.GITHUB_ID!,
+        clientSecret: event.platform.env?.GITHUB_SECRET!,
+      }),
+    ] as any,
     secret: event.platform.env?.AUTH_SECRET,
-    trustHost: true
-  }
-  return authOptions satisfies SvelteKitAuthConfig
-}) satisfies Handle;
+    trustHost: true,
+  };
+  return authOptions satisfies SvelteKitAuthConfig;
+});
